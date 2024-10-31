20404bdadc30b9cbe27ca843614c52a527915c0f
Zinc-zn <riskiafifudin05@gmail.com>
2024-10-31 09:14:28 +0700

    update1

--- ArrayRataNilai14.java
+++ ArrayRataNilai14.java
@@ -3,23 +3,32 @@ import java.util.Scanner;
 public class ArrayRataNilai14 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int[] nilaiMhs = new int[10];
-        double total = 0;
-        double rata2;
-        int jumlahLulus = 0;
+        double jumlahLulus = 0;
+        double jumlahTLulus = 0;
+        double totalLulus = 0;
+        double totalTLulus = 0;
+        
+        System.out.print("Masukkan jumlah mahasiswa : ");
+        int jumlahmahasiswa = input.nextInt();
+        int [] nilaiMhs= new int[jumlahmahasiswa];
 
         for (int i = 0; i < nilaiMhs.length; i++) {
-            System.out.print("Masukkan nilai mahasiswa ke-" + (i+1) + " : ");
+            System.out.print("Masukkan nilai mahasiswa ke-"+(i+1)+" : ");
             nilaiMhs[i] = input.nextInt();
         }
         for (int i = 0; i < nilaiMhs.length; i++) {
-            total += nilaiMhs[i];
-            if (nilaiMhs[i] > 70) {
+            if (nilaiMhs[i]>70){
+                totalLulus += nilaiMhs[i];
                 jumlahLulus++;
+            }else {
+                totalTLulus += nilaiMhs[i];
+                jumlahTLulus++;
             }
         }
-        rata2 = total/nilaiMhs.length;
-        System.out.println("Rata-rata nilai = " + rata2);
-        System.out.println("Jumlah mahasiswa yang lulus = " + jumlahLulus);
+        double rataLulus = totalLulus/jumlahLulus;
+        double rataTLulus = totalTLulus/jumlahTLulus;
+
+        System.out.println("Rata-rata nilai yang lulus = "+rataLulus);
+        System.out.println("Rata-rata nilai yang tidak lulus = "+rataTLulus);
     }
 }
--- SearchNilai14.java
+++ SearchNilai14.java
@@ -1,17 +1,30 @@
+import java.util.Scanner;
+
 public class SearchNilai14 {
     public static void main(String[] args) {
-        int [] arrNilai = {80,85,78,96,90,82,86};
-        int key=90;
+        Scanner input = new Scanner(System.in);
+        int key;
         int hasil=0;
 
+        System.out.print("Masukkan banyaknya nilai yang akan diinput : ");
+        int jumlah = input.nextInt();
+        int [] arrNilai = new int[jumlah];
+
+        for (int i = 0; i < arrNilai.length; i++) {
+            System.out.print("Masukkan nilai mahasiswa ke-"+(i+1)+" : ");
+            arrNilai[i] = input.nextInt();
+        }
+        System.out.print("Masukkan nilai yang ingin dicari : ");
+        key = input.nextInt();
         for (int i = 0; i < arrNilai.length; i++) {
             if(key==arrNilai[i]){
                 hasil=i;
+                System.out.println("\nNilai "+key+" ketemu, merupakan nilai mahasiswa ke-"+ (hasil+1));
+                break;
+            }else {
+                System.out.println("\nNilai yang dicari tidak ditemukan");
                 break;
             }
         }
-        System.out.println();
-        System.out.println("Nilai "+key+" key di indeks ke-"+hasil);
-        System.out.println();
     }
 }
